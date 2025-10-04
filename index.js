a4ae6d0f0c651ced692705c2f2045164e8a9a636
weeqeecombe50 <mynieagbona@hotmail.com>
2025-10-04 19:55:56 +0300

    Corrected redundant usage of 'version' in welcome message.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the English version of version control!');
+    res.send('Welcome to the English version of control!');
 });
 
 app.listen(PORT, () => {
